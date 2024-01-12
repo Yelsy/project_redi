a3dc9dcb4df027a25f560431d5da809dc2937775
yelsy <yelsycg17@gmail.com>
2024-01-12 01:52:52 -0500

    update route to news card

--- frontend/app/components/NewsCard.js
+++ frontend/app/components/NewsCard.js
@@ -1,12 +1,17 @@
-const NewsCard = ({ title, date, content, imageUrl }) => {
-    return (
+// components/NewsCard
+import Link from "next/link";
+
+const NewsCard = ({ title, imageUrl, newsId }) => {
+  return (
+    <Link href={`/news/${newsId}`}>
       <div className="border border-[#2E4053] bg-[#EAFAF1] rounded-lg bg-rounded-lg p-4 shadow-md m-5">
-        <img src={imageUrl} alt={title} className="w-full h-40 object-cover mb-4 rounded-t-lg" />
-        <h2 className="text-xl font-bold text-center mb-2">{title}</h2>
-        <p className="text-gray-700">{date}</p>
-        <p className="text-gray-700">{content}</p>
+        <img src={imageUrl} alt={title} className="w-full h-40 object-cover" />
+        <div className="p-4">
+          <h2 className="text-xl font-bold text-center mb-2">{title}</h2>
+        </div>
       </div>
-    );
-  };
-  
-  export default NewsCard;
+    </Link>
+  );
+};
+
+export default NewsCard;
