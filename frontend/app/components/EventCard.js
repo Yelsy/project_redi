3973979d59795614fcfb943a97b532042074a03a
yelsy <yelsycg17@gmail.com>
2024-01-12 01:52:23 -0500

    update route to event card

--- frontend/app/components/EventCard.js
+++ frontend/app/components/EventCard.js
@@ -1,12 +1,17 @@
-const EventCard = ({ title, date, location, imageUrl }) => {
-    return (
+// components/EventCard
+import Link from "next/link";
+
+const EventCard = ({ title, imageUrl, eventId }) => {
+  return (
+    <Link href={`/event/${eventId}`}>
       <div className="border border-[#2E4053] bg-[#EAFAF1] rounded-lg bg-rounded-lg p-4 shadow-md m-5">
-        <img src={imageUrl} alt={title} className="w-full h-40 object-cover mb-4 rounded-t-lg" />
-        <h2 className="text-xl font-bold text-center mb-2">{title}</h2>
-        <p className="text-gray-700">{date}</p>
-        <p className="text-gray-700">{location}</p>
+        <img src={imageUrl} alt={title} className="w-full h-40 object-cover" />
+        <div className="p-4">
+          <h2 className="text-xl font-bold text-center mb-2">{title}</h2>
+        </div>
       </div>
-    );
-  };
-  
-  export default EventCard;
+    </Link>
+  );
+};
+
+export default EventCard;
