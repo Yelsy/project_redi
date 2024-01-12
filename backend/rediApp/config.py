d050be03ceeafb756846ae7d7e798703f2ad474c
yelsy <yelsycg17@gmail.com>
2024-01-12 01:11:41 -0500

    delete

--- backend/rediApp/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    DEBUG = False
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:y123@localhost/redi1'
-    SECRET_KEY = 'your_secret_key'
-    JWT_SECRET_KEY = 'your_jwt_secret_key'
-
-class DevelopmentConfig(Config):
-    DEBUG = True
--- backend/rediApp/utils/patterns.py
+++
@@ -1,2 +0,0 @@
-PATTERN_EMAIL = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
-PATTERN_PASSWORD = r'.{6}'
